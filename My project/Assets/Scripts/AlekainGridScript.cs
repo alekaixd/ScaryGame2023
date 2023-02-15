5a5169b838b972cbf4b03e6dd68c3dded1d3d977
alekaixd <aleksi.pirttinen@gmail.com>
2023-02-15 10:50:46 +0200

    adddedd sexxx

---
+++ My project/Assets/Scripts/AlekainGridScript.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlekainGridScript : MonoBehaviour
+{
+    [SerializeField] private int _width, _height;
+
+    [SerializeField] private AlekainTileScript _tilePrefab;
+
+    [SerializeField] private Transform _cam;
+
+    private Dictionary<Vector2, AlekainTileScript> _tiles;
+
+    void Start()
+    {
+        GenerateGrid();
+    }
+
+    void GenerateGrid()
+    {
+        _tiles = new Dictionary<Vector2, AlekainTileScript>();
+        for (int x = 0; x < _width; x++)
+        {
+            for (int y = 0; y < _height; y++)
+            {
+                var spawnedTile = Instantiate(_tilePrefab, new Vector3(x, y), Quaternion.identity);
+                spawnedTile.name = $"Tile {x} {y}";
+
+                var isOffset = (x % 2 == 0 && y % 2 != 0) || (x % 2 != 0 && y % 2 == 0);
+                spawnedTile.Init(isOffset);
+
+
+                _tiles[new Vector2(x, y)] = spawnedTile;
+            }
+        }
+
+        _cam.transform.position = new Vector3((float)_width / 2 - 0.5f, (float)_height / 2 - 0.5f, -10);
+    }
+
+    public AlekainTileScript GetTileAtPosition(Vector2 pos)
+    {
+        if (_tiles.TryGetValue(pos, out var tile)) return tile;
+        return null;
+    }
+}
