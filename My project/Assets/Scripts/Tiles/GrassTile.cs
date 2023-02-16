bda34fa7d534050e183658642039b3783ad29e10
kukkakakka <111349064+kukkakakka@users.noreply.github.com>
2023-02-16 12:53:50 +0200

    rikkinäinen paska

--- My project/Assets/Scripts/Tiles/GrassTile.cs
+++ My project/Assets/Scripts/Tiles/GrassTile.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class GrassTile : AlekainTileScript
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private Color _baseColor, _offsetColor;
+
+    public override void Init(int x, int y)
     {
-        
+        var isOffset = (x + y) % 2 == 1;
+        _renderer.color = isOffset ? _offsetColor : _baseColor;
     }
 }
