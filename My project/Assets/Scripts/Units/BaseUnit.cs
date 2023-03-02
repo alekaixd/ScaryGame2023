0baae8659b61797a058ed628858de31bacc37c3a
alekaixd <aleksi.pirttinen@gmail.com>
2023-03-02 12:38:50 +0200

    Update BaseUnit.cs

--- My project/Assets/Scripts/Units/BaseUnit.cs
+++ My project/Assets/Scripts/Units/BaseUnit.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseUnit : MonoBehaviour
+public class BaseUnit : MonoBehaviour //  <--- Ahonen, pitääkö tän olla monobehaviour??
 {
     public string UnitName; //aika selkeä
     public AlekainTileScript OccupiedTile; //onko tile jo käytössä
